837401ace359dfbb9dfd75fee0461890f17883b5
sarrah yousfi <sarrahyousfi18@gmail.com>
2026-03-01 10:28:23 -1100

    Initial commit

---
+++ app/src/main/java/com/example/camping/Activity2.java
@@ -0,0 +1,35 @@
+package com.example.camping;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void previousScreen(View view) {
+        Intent intent = new Intent(this, Activity2.class);
+        startActivity(intent);
+    }
+
+    public void close(View view) {
+        finish();
+    }
+
+}
